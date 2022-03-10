01cbf58968e97be2b7cb2f39a8b6ce3ce3ce90f5
theoverse <theoverse@protonmail.com>
2022-03-10 07:24:55 -0600

    added avatar uploader functionality and single upload utility

---
+++ middlewares/users/avatarUpload.js
@@ -0,0 +1,10 @@
+function avatarUpload(req, res, next) {
+    const upload = uploader(
+        'avatars',
+        ['image/jpeg', 'image/jpg', 'image/png'],
+        10000000,
+        'Only .jpg, .jpeg, .png format allowed'
+    );
+}
+
+module.exports = avatarUpload;
---
+++ utilities/singleUploader.js
@@ -0,0 +1,11 @@
+function uploader(
+    subfolder_path,
+    allowed_file_types,
+    max_file_size,
+    error_message
+) {
+    // make Upload object
+    return upload;
+}
+
+module.exports = uploader;
